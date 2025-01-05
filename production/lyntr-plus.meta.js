5d50703bd4918bdd56b8a227883b7426133a905a
kiti-sites <103646253+kiti-sites@users.noreply.github.com>
2025-01-05 11:53:38 -0300

    Update lyntr-plus.meta.js

--- production/lyntr-plus.meta.js
+++ production/lyntr-plus.meta.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Lyntr+
 // @version      1.18.0
-// @github       https://github.com/Sylicium/lyntr-plus-userscript
+// @github       https://github.com/kiti-sites/lyntr-plus-userscript
 // @namespace    https://lyntr.com/
-// @description  A toolbox for small and medium changes for lyntr.com ! What is it ? -> https://youtu.be/-D2L3gHqcUA
+// @description  A toolbox for small and medium changes for lyntr.jnnj.xyz ! What is it ? -> https://youtu.be/-D2L3gHqcUA
 // @author       Sylicium
-// @match        https://lyntr.com/*
 // @match        https://lyntr.jnnj.xyz/*
 // @icon         https://lyntr.com/favicon.ico
 // @grant        none
