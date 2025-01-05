8352671a6403ac31b082bbe5047117332e8fdd83
kiti-sites <103646253+kiti-sites@users.noreply.github.com>
2025-01-05 11:53:07 -0300

    Update lyntr-plus.user.js

--- production/lyntr-plus.user.js
+++ production/lyntr-plus.user.js
@@ -1,16 +1,15 @@
 // ==UserScript==
 // @name         Lyntr+
 // @version      1.21.2
-// @github       https://github.com/Sylicium/lyntr-plus-userscript
+// @github       https://github.com/kiti-sites/lyntr-plus-userscript
 // @namespace    https://lyntr.com/
-// @description  A toolbox for small and medium changes for lyntr.com ! What is it ? -> https://youtu.be/-D2L3gHqcUA
+// @description  A toolbox for small and medium changes for lyntr.jnnj.xyz ! What is it ? -> https://youtu.be/-D2L3gHqcUA
 // @author       Sylicium
-// @match        https://lyntr.com/*
 // @match        https://lyntr.jnnj.xyz/*
 // @icon         https://lyntr.com/favicon.ico
 // @grant        none
-// @downloadURL https://raw.githubusercontent.com/Sylicium/lyntr-plus-userscript/main/production/lyntr-plus.user.js
-// @updateURL https://raw.githubusercontent.com/Sylicium/lyntr-plus-userscript/main/production/lyntr-plus.meta.js
+// @downloadURL https://raw.githubusercontent.com/kiti-sites/lyntr-plus-userscript/main/production/lyntr-plus.user.js
+// @updateURL https://raw.githubusercontent.com/kiti-sites/lyntr-plus-userscript/main/production/lyntr-plus.meta.js
 // ==/UserScript==
 
 (function() {
